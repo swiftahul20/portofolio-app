3dfae277ea070b26d58310ce434a456972ec5815
swiftah20 <sie.miftah@gmail.com>
2023-05-08 15:03:39 +0700

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+              About
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>
+              Hello I'm Miftahul Habib, nice to meet you. Please take a look around.
+            </p>
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+              unde dicta cupiditate perspiciatis quis eveniet nihil totam
+              temporibus, porro eius doloremque voluptas. Quo asperiores totam
+              ratione odit enim, fugiat optio.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={Avatar} alt="avatar" style={{ width: "50px" }} />
+        <img className="rounded-full" src={Avatar} alt="avatar" style={{ width: "50px" }} />
       </div>
 
       {/* menu  */}
